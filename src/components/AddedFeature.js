e75b91f41f43212edf3e4492bfa376c03f53d6f8
milklot <vbiostflex@gmail.com>
2021-02-28 11:59:12 -0500

    mvp

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeFeature } from "../actions/titleActions";
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        onClick={() => props.removeFeature(props.feature)}
+        className="button"
+        >X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = (state) => {
+  return {
+    additionalFeatures: state.additionalFeatures
+  }
+}
+
+export default connect(mapStateToProps, { removeFeature })(AddedFeature);
